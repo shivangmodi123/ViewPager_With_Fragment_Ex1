d2381d472aa2d9609c46612027e6111c3858e867
Android <shivangmodi123@gmail.com>
2022-07-24 12:28:01 +0530

    Initial Commit

--- app/src/main/java/com/example/viewpager_with_fragment_ex1/MainActivity.kt
+++ app/src/main/java/com/example/viewpager_with_fragment_ex1/MainActivity.kt
@@ -1,14 +1,68 @@
 package com.example.viewpager_with_fragment_ex1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.Nullable
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // shivang
-        hekki
+        val tab_toolbar = findViewById<Toolbar>(R.id.toolbar)
+        val tab_viewpager = findViewById<ViewPager>(R.id.tab_viewpager)
+        var tab_tablayout = findViewById<TabLayout>(R.id.tab_tablayout)
+
+        setSupportActionBar(tab_toolbar)
+        setupViewPager(tab_viewpager)
+    }
+
+    private fun setupViewPager(viewpager: ViewPager) {
+        val adapter: ViewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+
+        // LoginFragment is the name of Fragment and the Login
+        // is a title of tab
+        adapter.addFragment(LoginFragment(), "Login")
+        adapter.addFragment(SignupFragment(), "Signup")
+
+        // setting adapter to view pager.
+        viewpager.adapter = adapter
+    }
+
+    class ViewPagerAdapter// this is a secondary constructor of ViewPagerAdapter class.
+        (supportFragmentManager: FragmentManager) : FragmentPagerAdapter(supportFragmentManager) {
+
+        // objects of arraylist. One is of Fragment type and
+        // another one is of String type.*/
+        private var fragmentList1: ArrayList<Fragment> = ArrayList()
+        private var fragmentTitleList1: ArrayList<String> = ArrayList()
+
+        // returns which item is selected from arraylist of fragments.
+        override fun getItem(position: Int): Fragment {
+            return fragmentList1.get(position)
+        }
+
+        // returns which item is selected from arraylist of titles.
+        @Nullable
+        override fun getPageTitle(position: Int): CharSequence {
+            return fragmentTitleList1.get(position)
+        }
+
+        // returns the number of items present in arraylist.
+        override fun getCount(): Int {
+            return fragmentList1.size
+        }
+
+        // this function adds the fragment and title in 2 separate  arraylist.
+        fun addFragment(fragment: Fragment, title: String) {
+            fragmentList1.add(fragment)
+            fragmentTitleList1.add(title)
+        }
     }
 }
